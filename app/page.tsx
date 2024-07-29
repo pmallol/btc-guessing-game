09b35b7dd0c4c0bf28934b30a2a122dec6d1e86b
pmallol <paulamallol@gmail.com>
2024-07-28 22:55:24 -0300

    Update GuessButtons component to handle user guesses and loading state

--- app/page.tsx
+++ app/page.tsx
@@ -15,8 +15,7 @@ export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
       <div className="z-10 w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex flex flex-col gap-y-8">
-        <BTCPrice />
-        <GuessButtons />
+        <GuessButtons onGuess={handleGuess} loading={loading}/>
         <Score userId={userId || ""}/>
       </div>
     </main>
--- components/GuessButtons.tsx
+++ components/GuessButtons.tsx
@@ -1,18 +1,28 @@
 "use client"
-import { useState } from 'react';
 
-const GuessButtons = () => {
-  const [guess, setGuess] = useState<'up' | 'down' | null>(null);
+interface GuessButtonsProps {
+  onGuess: (guess: 'up' | 'down') => void;
+  loading: boolean;
+}
 
-  const handleGuess = (direction: 'up' | 'down') => {
-    setGuess(direction);
-    // Save guess to backend
-  };
+const GuessButtons: React.FC<GuessButtonsProps> = ({ onGuess, loading }) => {
 
   return (
     <div className='flex gap-4'>
-      <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded" onClick={() => handleGuess('up')} disabled={guess !== null}>Up</button>
-      <button className="bg-red-500 hover:bg-red-700 text-white font-bold py-2 px-4 rounded" onClick={() => handleGuess('down')} disabled={guess !== null}>Down</button>
+      <button
+        className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded disabled:bg-blue-900" 
+        onClick={() => onGuess('up')}
+        disabled={loading}
+        >
+          Up
+        </button>
+      <button
+        className="bg-red-500 hover:bg-red-700 text-white font-bold py-2 px-4 rounded disabled:bg-red-900" 
+        onClick={() => onGuess('down')} 
+        disabled={loading}
+        >
+          Down
+        </button>
     </div>
   );
 };

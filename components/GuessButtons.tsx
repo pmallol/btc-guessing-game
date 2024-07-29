9165e56f36357938d620e4e03b1f4c6f6507eb7d
pmallol <paulamallol@gmail.com>
2024-07-28 23:56:00 -0300

    Update GuessButtons component styling and disable button text when loading

--- components/GuessButtons.tsx
+++ components/GuessButtons.tsx
@@ -10,14 +10,14 @@ const GuessButtons: React.FC<GuessButtonsProps> = ({ onGuess, loading }) => {
   return (
     <div className='flex gap-4'>
       <button
-        className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded disabled:bg-blue-900" 
+        className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded disabled:bg-blue-900 disabled:text-gray-400" 
         onClick={() => onGuess('up')}
         disabled={loading}
         >
           Up
         </button>
       <button
-        className="bg-red-500 hover:bg-red-700 text-white font-bold py-2 px-4 rounded disabled:bg-red-900" 
+        className="bg-red-500 hover:bg-red-700 text-white font-bold py-2 px-4 rounded disabled:bg-red-900 disabled:text-gray-400" 
         onClick={() => onGuess('down')} 
         disabled={loading}
         >

2f346271fc01afd2bf645bfea8181740a42844df
pmallol <paulamallol@gmail.com>
2024-07-28 22:56:49 -0300

    Update BTCPrice component to use fetchBTCPrice from services/BTCPrice

--- app/page.tsx
+++ app/page.tsx
@@ -1,20 +1,41 @@
-import { cookies } from 'next/headers'
+"use client"
+import { useState, useEffect } from 'react';
+import { fetchBTCPrice } from '../services/BTCPrice';
 
-import BTCPrice from "@/components/BTCPrice";
 import Score from "@/components/Score";
 import GuessButtons from "@/components/GuessButtons";
 
 function getUserId() {
-  const userId = cookies().get('userId')?.value
-  return userId ? userId : ''
+  const cookies = document.cookie;
+  const userId = cookies?.split('; ').find(cookie => cookie.startsWith('userId='));
+  return userId ? userId.split('=')[1] : ''
 }
 
 export default function Home() {
   const userId = getUserId();
 
+  const [loading, setLoading] = useState(false);
+  const [error, setError] = useState<string | null>(null);
+  const [btcPrice, setBtcPrice] = useState<number | null>(null);
+  const [userScore, setUserScore] = useState<number | null>(null);
+
+  useEffect(() => {
+    const fetchInitialPrice = async () => {
+      try {
+        const price = await fetchBTCPrice();
+        setBtcPrice(price);
+      } catch (error) {
+        setError('Error occurred while fetching initial BTC price');
+      }
+    };
+
+    fetchInitialPrice();
+  }, []);
+
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
       <div className="z-10 w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex flex flex-col gap-y-8">
+        {btcPrice && <div>Current BTC Price: ${btcPrice}</div>}
         <GuessButtons onGuess={handleGuess} loading={loading}/>
         <Score userId={userId || ""}/>
       </div>

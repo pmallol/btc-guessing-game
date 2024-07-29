700981105e098db53d39352c789a53da95a485f8
pmallol <paulamallol@gmail.com>
2024-07-28 21:37:37 -0300

    Update Score component to fetch user score from DynamoDB

--- app/page.tsx
+++ app/page.tsx
@@ -1,14 +1,23 @@
+import { cookies } from 'next/headers'
+
 import BTCPrice from "@/components/BTCPrice";
 import Score from "@/components/Score";
 import GuessButtons from "@/components/GuessButtons";
 
+function getUserId() {
+  const userId = cookies().get('userId')?.value
+  return userId ? userId : ''
+}
+
 export default function Home() {
+  const userId = getUserId();
+
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
       <div className="z-10 w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex flex flex-col gap-y-8">
         <BTCPrice />
         <GuessButtons />
-        <Score />
+        <Score userId={userId || ""}/>
       </div>
     </main>
   );
--- components/Score.tsx
+++ components/Score.tsx
@@ -1,8 +1,36 @@
 "use client"
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 
-const Score = () => {
-  const [score, setScore] = useState(0);
+interface ScoreProps {
+  userId: string;
+}
+
+const Score: React.FC<ScoreProps> = ({ userId }) => {
+  const [score, setScore] = useState<number | null>(null);
+  const [loading, setLoading] = useState<boolean>(true);
+
+  useEffect(() => {
+    const fetchScore = async () => {
+      try {
+        const response = await fetch(`/api/score?userId=${userId}`);
+        if (!response.ok) {
+          throw new Error('Failed to fetch score');
+        }
+        const data = await response.json();
+        setScore(data.score);
+      } catch (error) {
+        console.error('Error fetching score:', error);
+      } finally {
+        setLoading(false);
+      }
+    };
+
+    fetchScore();
+  }, [userId]);
+
+  if (loading) {
+    return <div>Loading...</div>;
+  }
 
   return <div className="text-center text-2xl font-bold">Score: {score}</div>;
 };

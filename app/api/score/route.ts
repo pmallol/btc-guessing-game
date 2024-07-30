4a2c251bc7bbf7fd6596606eca804bcb01b9faaa
pmallol <paulamallol@gmail.com>
2024-07-29 22:14:13 -0300

    Update Next.js API route to use NextRequest and implement POST request

--- app/api/score/route.ts
+++ app/api/score/route.ts
@@ -1,6 +1,8 @@
-import type { NextApiRequest, NextApiResponse } from 'next'
+import type { NextApiResponse } from 'next'
+import { type NextRequest } from 'next/server'
 import { DynamoDBDocument, GetCommand, UpdateCommand } from '@aws-sdk/lib-dynamodb';
 import { DynamoDB } from '@aws-sdk/client-dynamodb';
+import { ReturnValue } from '@aws-sdk/client-dynamodb';
 
 // Initialize DynamoDB Document Client
 const dynamoDB = DynamoDBDocument.from(new DynamoDB({
@@ -16,7 +18,7 @@ type Data = {
   message?: string;
 };
 
-export async function GET(request: NextApiRequest, res: NextApiResponse<Data>) {
+export async function GET(request: NextRequest, res: NextApiResponse<Data>) {
   const { searchParams } = new URL(request.url ?? '')
   const userId = searchParams.get('userId')
   if (!userId) {
@@ -24,6 +26,22 @@ export async function GET(request: NextApiRequest, res: NextApiResponse<Data>) {
   }
 
   return await getUserScore(userId as string, res);
+}
+
+export async function POST(request: NextRequest, res: NextApiResponse<Data>) {
+  const { searchParams } = new URL(request.url ?? '')
+  const userId = searchParams.get('userId')
+  if (!userId) {
+    return Response.json({ message: 'User ID is required' }, { status: 400 });
+  }
+
+  const body = await request.json()
+  const { timestamp, guess } = body
+  if (typeof timestamp !== 'number' || typeof guess !== 'boolean') {
+    return Response.json({ message: 'Invalid request body' }, { status: 400 });
+  }
+
+  return await updateUserScore(userId as string, timestamp, guess, res);
 }
 
 const getUserScore = async (userId: string, res: NextApiResponse<Data>) => {
@@ -60,5 +78,28 @@ const setUserScore = async (userId: string, timestamp: number, score: number, re
     return Response.json({ score: newScore }, { status: 200 })
   } catch (error) {
     return Response.json({ message: `Error updating score: ${error}` }, { status: 500 });
+  }
+};
+
+const updateUserScore = async (userId: string, timestamp: number, guess: boolean, res: NextApiResponse<Data>) => {
+  const scoreChange = guess ? 1 : -1;
+  const updateParams = {
+    TableName: 'BTC-guessing-game-scores',
+    Key: { userId },
+    UpdateExpression: 'SET score = if_not_exists(score, :start) + :change',
+    ExpressionAttributeValues: {
+      ':start': 0,
+      ':change': scoreChange,
+    },
+    ReturnValues: ReturnValue.ALL_NEW, // Use the correct enum value
+  };
+
+  try {
+    const updated = await dynamoDB.send(new UpdateCommand(updateParams));
+    const newScore = updated.Attributes?.score || '0';
+
+    return Response.json({ score: parseInt(newScore) }, {status: 200});
+  } catch (error) {
+    return Response.json({ message: `Error updating score: ${error}` }, {status: 500});
   }
 };

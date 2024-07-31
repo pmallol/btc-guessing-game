176892efe77b58607c64691a36523bf7eca58d98
pmallol <paulamallol@gmail.com>
2024-07-30 22:21:52 -0300

    Update Home test to fetch user score and update btc Price on button click

--- __tests__/Home.test.tsx
+++ __tests__/Home.test.tsx
@@ -22,6 +22,7 @@ vi.mock('../services/BTCPrice', () => ({
   fetchBTCPrice: vi.fn(),
 }));
 
+vi.spyOn(global, 'fetch').mockImplementation(vi.fn());
 
 describe('Home', () => {
   beforeEach(() => {
@@ -42,5 +43,54 @@ describe('Home', () => {
   it('displays the current BTC price', async () => {
     render(<Home />);
     await waitFor(() => expect(screen.getByTestId('btc-price')).toBeInTheDocument());
+  });
+
+  it('fetches the user score and updates btcPricePrev on button click', async () => {
+    const mockInitialPrice = 22000;
+    const mockNewPrice = 22100;
+    const userScore = 5;
+  
+    // Mock fetchBTCPrice
+    vi.mocked(fetchBTCPrice).mockResolvedValue(mockInitialPrice)
+      .mockResolvedValueOnce(mockNewPrice);
+
+    // Mock fetch for API
+    (global.fetch as jest.Mock).mockImplementationOnce(async (url: string | string[]) => {
+      if (url.includes('/api/score?userId=test-user-id')) {
+        return {
+          ok: true,
+          json: async () => ({ score: userScore }),
+        };
+      }
+      return {
+        ok: true,
+        json: async () => ({}),
+      };
+    });
+
+    render(<Home />);
+
+    // Simulate button click
+    fireEvent.click(screen.getByText('Down'));
+
+    expect(fetchBTCPrice).toHaveBeenCalledTimes(2);
+
+    // Verify that fetch call to /api/score was made with correct userId and timestamp
+    await waitFor(() => {
+      const fetchCalls = (global.fetch as unknown as jest.Mock).mock.calls;
+      // console.log('fetchCalls', fetchCalls, fetchCalls[0][0]);
+      expect(fetchCalls[0][0]).toBe('/api/score?userId=test-user-id');
+      const requestBody = JSON.parse(fetchCalls[1][1].body as string);
+      expect(typeof requestBody.timestamp).toBe('number'); // Check if timestamp is a number
+      expect(requestBody.guess).toBe(false);
+    });
+    // Verify if BTC Price was updated
+    await waitFor(() => {
+      expect(screen.getByTestId('btc-price')).toBeInTheDocument();
+      expect(screen.getByTestId('btc-price')).toHaveTextContent(mockNewPrice.toString());
+    });
+
+    // Verify score update
+    expect(await screen.getByTestId('score')).toHaveTextContent('Score: 5');
   });
 });

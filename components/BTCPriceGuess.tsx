b5646b2869e038bf01f396f86c7f225466427449
pmallol <paulamallol@gmail.com>
2024-07-31 18:19:12 -0300

    Refactor BTCPriceGuess component and improve formatting of text

--- components/BTCPriceGuess.tsx
+++ components/BTCPriceGuess.tsx
@@ -10,18 +10,42 @@ interface BTCPriceGuessProps {
 }
 
 const BTCPriceGuess: React.FC<BTCPriceGuessProps> = ({ btcPrice, btcPricePrev, error, userScore, userGuess, hideMessage }) => {
+  const formatPrice = (price: number | null): string => {
+    if (price === null) {
+      return '';
+    }
+    return new Intl.NumberFormat('en-US', {
+      style: 'currency',
+      currency: 'USD',
+    }).format(price);
+  };
+
   return (
-    <div className='flex flex-col items-center self-center text-lg container border border-orange-300 w-full h-64 p-12 py-20 drop-shadow-md rounded-lg'>
-      {!btcPrice && <div data-testid="btc-price-loading">Loading BTC Price...</div>}
-      {btcPrice && <div data-testid="btc-price">Current BTC Price: <b>${btcPrice}</b></div>}
-      {btcPricePrev && <div data-testid="btc-price-prev" className='mt-4 text-gray-600'>Previous BTC Price: <b>${btcPricePrev}</b></div>}
+    <div className='flex flex-col self-center text-lg container border border-orange-300 w-full h-64 p-12 py-20 drop-shadow-md rounded-lg'>
+      <div className='flex justify-between gap-8'>
+        {!btcPrice && <div data-testid="btc-price-loading">Loading BTC Price...</div>}
+        {btcPrice &&
+          <div data-testid="btc-price">
+            Latest BTC Price:
+            <p><b>{formatPrice(btcPrice)}</b>
+              {btcPricePrev && <span className='text-sm ml-3 text-orange-400'> {btcPrice > btcPricePrev ? '+' : ''}{formatPrice(btcPrice - btcPricePrev)}</span>}
+            </p>
+          </div>
+        }
+        {btcPricePrev &&
+          <div data-testid="btc-price-prev" className='text-gray-500'>Previous BTC Price:
+            <p><b>{formatPrice(btcPricePrev)}</b></p>
+          </div>
+        }
+      </div>
 
       {!error && userScore !== null && (
         <div className={`mt-8 ${hideMessage ? 'hidden' : ''}`}>
           {userGuess ? (
             <div className='text-sm'>✅ YAY! You guessed right, the price went {btcPrice && btcPricePrev ? (btcPrice > btcPricePrev ? 'up' : 'down') : ''}.</div>
           ) : (
-            <div className='text-sm'>❌ Oops! You didn&apos;t guess this time. Try again!</div>
+            <div className='text-sm'>❌ You didn&apos;t guess this time, the price {btcPrice === btcPricePrev ? 'stayed the same' :
+            `went ${btcPrice && btcPricePrev ? (btcPrice > btcPricePrev ? 'up' : 'down') : ''}`}. Try again!</div>
           )}
         </div>
       )}

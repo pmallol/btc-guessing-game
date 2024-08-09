a252853b107a8759f70e748d2578a4f74f0b92e4
pmallol <paulamallol@gmail.com>
2024-08-09 02:26:50 -0300

    Fix indentation in btc-price api route

--- app/api/btc-price/route.ts
+++ app/api/btc-price/route.ts
@@ -10,16 +10,16 @@ export async function GET(request: NextRequest, res: NextApiResponse<Data>) {
   const headers = new Headers();
   headers.append('X-CMC_PRO_API_KEY', process.env.N_COIN_MARKET_API!);
 
- try {
-  const response = await fetch('https://pro-api.coinmarketcap.com/v1/cryptocurrency/quotes/latest?symbol=BTC', {
-    method: 'GET',
-    headers: headers,
-    cache: 'no-store'
-  });
-  const data = await response.json();
+  try {
+    const response = await fetch('https://pro-api.coinmarketcap.com/v1/cryptocurrency/quotes/latest?symbol=BTC', {
+      method: 'GET',
+      headers: headers,
+      cache: 'no-store'
+    });
+    const data = await response.json();
 
-  return Response.json({price: data.data.BTC.quote.USD.price}, {status: 200});
- } catch (error) {
-  return Response.json({ message: `Error fetching BTC price: ${error}` }, {status: 500});
- }
+    return Response.json({ price: data.data.BTC.quote.USD.price }, { status: 200 });
+  } catch (error) {
+    return Response.json({ message: `Error fetching BTC price: ${error}` }, { status: 500 });
+  }
 }
